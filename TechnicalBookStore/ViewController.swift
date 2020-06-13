9783d348604c45aef5332d1c45f87b15766864f4
popai306 <fttttttttf@gmail.com>
2020-06-13 13:56:22 +0900

    Initial Commit

---
+++ TechnicalBookStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechnicalBookStore
+//
+//  Created by Tomohiro Fukuya on 2020/06/13.
+//  Copyright © 2020 Tomohiro Fukuya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
